3f2fd9fc80f0940315093415c73f81a80a8f63b6
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2022-10-12 00:53:16 +0530

    created To-Do Main page and backward navigation

---
+++ app/src/main/java/com/example/empressnotes/ToDoMain.java
@@ -0,0 +1,27 @@
+package com.example.empressnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class ToDoMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_main);
+
+        //set back button in action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //set action bar title
+        getSupportActionBar().setTitle("My To-Do");
+
+        //set action bar Color
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#3FA796"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
+    }
+}
