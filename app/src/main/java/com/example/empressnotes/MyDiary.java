c812dae53efa54845e1b30eef1e1b551a418b2af
anusaraweerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2022-10-12 18:02:11 +0530

    Initial home page for diary app.

--- app/src/main/java/com/example/empressnotes/MyDiary.java
+++ app/src/main/java/com/example/empressnotes/MyDiary.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MyDiary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-
-
+        setContentView(R.layout.activity_my_diary);
     }
-
 }
