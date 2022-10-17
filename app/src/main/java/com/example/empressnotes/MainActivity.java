7b51ce2f734929e2fee34f000ccb7b8757566cfe
Anusara Weerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2022-10-17 14:18:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/empressnotes/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.empressnotes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout layoutDiaryMain, layoutToDoMain, layoutNotesMain, layoutListsMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Hide action bar
+        getSupportActionBar().hide();
+
+
+        //onclick My Diary tab
+        layoutDiaryMain = findViewById(R.id.layoutDiaryMain);
+        layoutDiaryMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(
+                        new Intent(MainActivity.this, MyDiary.class)
+                );
+            }
+        });
+
+
+        //onclick My To-Do tab
+        layoutToDoMain = findViewById(R.id.layoutToDoMain);
+        layoutToDoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(
+                        new Intent(MainActivity.this, ToDoMain.class)
+                );
+            }
+        });
+
+        //onclick My Notes tab
+        layoutNotesMain = findViewById(R.id.layoutNotesMain);
+        layoutNotesMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(
+                        new Intent(MainActivity.this, MyNotes.class)
+                );
+            }
+        });
+
+        //onclick My Lists tab
+        layoutListsMain = findViewById(R.id.layoutListsMain);
+        layoutListsMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(
+                        new Intent(MainActivity.this, MyList.class)
+                );
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/empressnotes/MyList.java
@@ -0,0 +1,26 @@
+package com.example.empressnotes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_list);
+
+        //set back button in action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //set action bar title
+        getSupportActionBar().setTitle("Empress Notes");
+
+        //set action bar Color
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FF3700B3"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+    }
+}
