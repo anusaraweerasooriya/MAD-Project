f5387cc8b43d456341bc7b5d9f2f5bc23c6d5d2c
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2022-10-17 22:00:52 +0530

    database configurations

--- app/src/main/java/com/example/empressnotes/adapters/DatabaseHelper.java
+++ app/src/main/java/com/example/empressnotes/adapters/DatabaseHelper.java
@@ -35,6 +35,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_DIARY_IMAGE_PATH = "imagePath";
 
     // my_todo table columns
+    private static final String COLUMN_TASK_ID = "id";
+    private static final String COLUMN_TASK_TITLE = "title";
+    private static final String COLUMN_TASK_DESCRIPTION = "description";
+    private static final String COLUMN_TASK_DATE = "date";
+    private static final String COLUMN_TASK_TIME = "time";
+    private static final String COLUMN_TASK_WEB_LINK = "web_link";
 
     // my_notes table columns
 
@@ -56,6 +62,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(my_diary_query);
 
         // My To-Do--------------------------------------
+        String todo_query = " CREATE TABLE " + TABLE_NAME2 +
+                "(" + COLUMN_TASK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TASK_TITLE + " TEXT, " +
+                COLUMN_TASK_DESCRIPTION + " TEXT, " +
+                COLUMN_TASK_DATE + " TEXT, " +
+                COLUMN_TASK_TIME + " TEXT, " +
+                COLUMN_TASK_WEB_LINK + " TEXT);";
+
+        sqLiteDatabase.execSQL(todo_query);
 
         // My Notes--------------------------------------
 
@@ -71,6 +86,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
 
         // My To-Do
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME2);
+        onCreate(sqLiteDatabase);
 
         // My Notes
 
